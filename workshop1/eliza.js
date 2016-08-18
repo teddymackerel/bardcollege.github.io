8c1fde7c5bfd43c2139f9ab4fca7e6b6e3c91b68
teddymackerel <am4359@bard.edu>
2016-08-18 15:24:31 -0400

    Update eliza.js

--- workshop1/eliza.js
+++ workshop1/eliza.js
@@ -31,6 +31,9 @@ var rules = {
   "~*x hello ~*y": [
     "How do you do. Please state your problem."
   ],
+  "~*x updog ~*y": [
+  	"What is updog?",
+  ],
   "~*x computer ~*y": [
     "Do computers worry you?",
     "What do you think about machines have to do with your problem?",
